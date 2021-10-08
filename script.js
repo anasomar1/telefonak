4a1dbc495b12f50f5f8fed3084267aa0a2710966
Anas Yaseen <Anasyaseen1@outlook.com>
2021-10-08 11:28:34 +0300

    Extra event listener to close menu on link click, styling changes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.querySelector(".bars-menu").addEventListener("click", () => {
   document.querySelector(".nav-items").classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelector(".nav-items").classList.toggle("active");
+  });
+});
